2a7f5addb26a2bb991e51f0e480699b22af1876e
Jeff Bachtel <jeff.bachtel@ironnetcybersecurity.com>
2019-10-18 16:56:50 -0600

    directly plug values into k8s auth

--- provider.tf
+++ provider.tf
@@ -18,7 +18,11 @@ provider "helm" {
   insecure = true
 
   kubernetes {
-    config_path = local_file.kube_cluster_yaml.filename
+    host                   = local.api_server_url
+    client_certificate     = rke_cluster.rancher_server.client_cert
+    client_key             = rke_cluster.rancher_server.client_key
+    cluster_ca_certificate = rke_cluster.rancher_server.ca_crt
+    insecure               = true
   }
 }
 
